8cedb997ecc3818c1bcb1cceb0a520cbabd7722d
Ambiente 106 <ambiente106@iMac-de-CADPH.local>
2025-11-27 17:31:14 -0500

    julio

--- backend/controllers/perfil.js
+++ backend/controllers/perfil.js
@@ -18,6 +18,7 @@ export const obtenerPerfil = async (req, res) => {
             usuario:{
                 id:usuario._id,
                 nombre:usuario.nombre,
+                apellido:usuario.apellido,
                 email:usuario.email,
                 telefono:usuario.telefono
             }
@@ -26,3 +27,86 @@ export const obtenerPerfil = async (req, res) => {
         res.status(500).json({message:"Error al obtener el perfil", error:error.message});
     }
 }
+// Actualizar perfil del usuario
+export const actualizarPerfil = async (req, res) => {
+    try {
+        const { email, nombre, apellido, telefono } = req.body;
+
+        //validar campos obligatorios
+        if(!email) {
+            return res.status(400).json({ message: "Email es requerido" });
+        }
+
+        if(!nombre || !apellido || !telefono){
+            return res.status(400).json({ message: "Todos los campos son obligatorios" });
+        }
+
+        //Buscar y actualizar usuario
+        const usuarioActualizado = await user.findOneAndUpdate(
+            { email: email },
+            {
+                nombre: nombre,
+                apellido: apellido,
+                telefono: telefono
+            },
+            { new: true }
+            // No va seleccionar el campo passwords
+        ).select('-password');
+
+        if(!usuarioActualizado){
+            return res.status(400).json({ message: "Usuario no encontrado" });
+        }
+
+        res.status(200).json({
+            message: "Perfil actualizado exitosamente",
+            usuario: {
+                id: usuarioActualizado._id,
+                nombre: usuarioActualizado.nombre,
+                apellido: usuarioActualizado.apellido,
+                email: usuarioActualizado.email,
+                telefono: usuarioActualizado.telefono
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al actualizar el perfil",
+            error: error.message
+        });
+    }
+};
+// Eliminar perfil
+export const eliminarPerfil = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        //Validar que el email este presente
+        if(!email){
+            return res.status(400).json({ message: "Email es requerido" });
+        } 
+
+        //Buscar y eliminar usuario 
+        const usuarioEliminado = await user.findOneAndDelete({
+            email: email
+        });
+
+        if(!usuarioEliminado){
+            return res.status(400),json({ menssage: "Usuario no encontrado" });
+        }
+
+        res.status(200).json({
+            message: "Usuario eliminado exitosamente",
+            usuario: {
+                id: usuarioEliminado._id,
+                nombre: usuarioEliminado.nombre,
+                apellido: usuarioEliminado.apellido,
+                email: usuarioEliminado.email,
+                telefono: usuarioEliminado.telefono
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al eliminar perfil",
+            error: error.message
+        });
+    }
+};
